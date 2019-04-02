1af21efe48da34e303fca949d9357a780529d201
Philip Tenteromano <tenteromano.p@gmail.com>
2019-04-02 16:38:08 -0400

    added gcd

---
+++ gcd.js
@@ -0,0 +1,36 @@
+// Phil Tenteromano
+
+// GCD algorithm in javascript
+
+// gcd === 8
+const num1 = 80;
+const num2 = 24;
+
+function gcd(a, b) {
+  // if equal, return
+  if (a === b) return a;
+
+  // 'a' should be the larger number
+  if (a < b) {
+    let temp = a;
+    a = b;
+    b = temp;
+  }
+
+  // remainder
+  let r;
+
+  while (true) {
+    r = a % b;
+    a = b;
+    b = r;
+    // no remainder means full-division, gcd found
+    if (r === 0) return a;
+  }
+}
+
+console.log(gcd(num1, num2));
+
+module.exports = {
+  gcd
+};
